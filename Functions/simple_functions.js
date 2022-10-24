32c1117019004ca75ea2754af79af8bc9887256b
Willian Arenas <williamarenas07@gmail.com>
2022-10-24 13:19:35 -0400

    exercise 1

---
+++ Functions/simple_functions.js
@@ -0,0 +1,25 @@
+
+
+function displayfavoriteThings() {
+    let name = "Willian A";
+    let movie = "The Lion King";
+    let musician = "anything";
+
+    console.log("Hi my name is " + name + ", my favorite movie is " + movie + " and I listen to the " + musician + ".")
+
+}
+
+
+function whyImHere() {
+    console.log("The reason I joined this learning program is because I always wanted to go into the tech field and I figure programming would be the best area.")
+}
+
+
+function favoritePlace() {
+    console.log("One of my favorite places to visit is Mexico, I've only been a few times but it's always nice seeing family especially the ones I can't see often.");
+}
+
+
+displayfavoriteThings();
+whyImHere();
+favoritePlace();
